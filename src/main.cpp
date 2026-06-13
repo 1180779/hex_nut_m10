c529af723bd1127245b789336692ae30bf2b915d
1180779 <rdkgsk@gmail.com>
2026-06-13 16:20:06 +0200

    Add pocket with sketch and Boolean cut

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,9 @@
 #include <BRepBuilderAPI_MakeFace.hxx>
 #include <BRepBuilderAPI_MakeWire.hxx>
 #include <BRepBuilderAPI_Transform.hxx>
+#include <BRepAlgoAPI_Cut.hxx>
 #include <gp_Pnt.hxx>
+#include <gp_Circ.hxx>
 #include <gp_Trsf.hxx>
 #include <cmath>
 #include <stdexcept>
@@ -47,17 +49,41 @@ TopoDS_Shape pad(const TopoDS_Face &sketch, const float height) {
     return BRepPrimAPI_MakePrism(centered, gp_Vec(0, 0, height));
 }
 
+TopoDS_Face makePocketSketch(const float radius) {
+    const gp_Pnt center{};
+    const gp_Dir unitZ{0.0f, 0.0f, 1.0f};
+    const gp_Ax2 axis(center, unitZ);
+    gp_Circ circle{axis, radius};
+
+    BRepBuilderAPI_MakeEdge edgeMaker(circle);
+    TopoDS_Edge edge = edgeMaker.Edge();
+
+    BRepBuilderAPI_MakeWire wireBuilder;
+    wireBuilder.Add(edge);
+    ASSERT_DONE(wireBuilder);
+
+    return BRepBuilderAPI_MakeFace(wireBuilder);
+}
+
+TopoDS_Shape pocket(const TopoDS_Shape &base, const TopoDS_Face &sketch, const float depth) {
+    const TopoDS_Shape tool = pad(sketch, depth);
+    return BRepAlgoAPI_Cut(base, tool).Shape();
+}
+
 int main() {
     const HexNutParams params{};
 
     const TopoDS_Face hexSketch = makeHexSketch(params.getE() / 2.0f);
     const TopoDS_Shape hexShape = pad(hexSketch, params.getM());
 
-    const BRepMesh_IncrementalMesh mesh(hexShape, 0.1);
+    const TopoDS_Face pocketSketch = makePocketSketch(params.getD() / 2.0f);
+    const TopoDS_Shape result = pocket(hexShape, pocketSketch, params.getM());
+
+    const BRepMesh_IncrementalMesh mesh(result, 0.1); //, false, 0.1);
     ASSERT_DONE(mesh);
 
     StlAPI_Writer hexWriter;
-    hexWriter.Write(hexShape, "hex.stl");
+    hexWriter.Write(result, "hex.stl");
     std::cout << "DONE" << std::endl;
     return 0;
 }
